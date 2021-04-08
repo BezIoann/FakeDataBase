ca8a57d2b992b5c6079ea1e027e54e5c5b59d592
Ioann <rus.vanyok@gmail.com>
2021-04-08 20:24:26 +0300

    Add csv-formatter

--- src/DataBase/Main.java
+++ src/DataBase/Main.java
@@ -1,7 +1,8 @@
 package DataBase;
 import com.github.javafaker.Faker;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.*;
 
 public class Main {
@@ -10,11 +11,10 @@ public class Main {
         try {
             int numberOfRecords = Integer.parseInt(args[0]);
             int n = numberOfRecords;
-            String[] data = new String[numberOfRecords];
+            String[][] data = new String[numberOfRecords][3];
             for(int i = 0; numberOfRecords % 4 > 0; ++i) {
                 ++numberOfRecords;
             }
-            long m = System.currentTimeMillis();
             String region = args[1];
             if (region.equals("en-US") || region.equals("ru-RU") || region.equals("uk-UA")) {
 
@@ -23,22 +23,22 @@ public class Main {
                 System.exit(0);
             }
             Faker usFaker = new Faker(new Locale(region));
-
             (new firstThread(data, usFaker, numberOfRecords, region)).start();
             (new secondThread(data, usFaker, numberOfRecords, region)).start();
             (new thridThread(data, usFaker, numberOfRecords, region)).start();
             for(int i = n*3/4; i < n; i++) {
                 data[i] = fakeDataUnit(usFaker,region);
             }
-            System.out.println("Time for generate DataBase: " + (double) (System.currentTimeMillis() - m));
+            CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.RFC4180.withHeader().withDelimiter(';'));
             for(int i = 0; i < n; i++) {
-                System.out.println(data[i]);
+                printer.printRecord(data[i]);
             }
         } catch (Exception e) {
             System.exit(0);
         }
     }
-    public static String fakeDataUnit(Faker usFaker, String loc) {
+    public static String[] fakeDataUnit(Faker usFaker, String loc) throws IOException {
+
         String name = usFaker.name().fullName();
         String addr = usFaker.address().fullAddress();
         String fullAddress =  addr;
@@ -50,16 +50,17 @@ public class Main {
         } else if (loc.equals("uk-UA")) {
             fullAddress = (Math.random() > 0.5) ? addr + " Україна" : addr + " УКР";
         }
-        return  name + "; " + fullAddress + "; " +  usFaker.phoneNumber().cellPhone();
+
+        return  new String[]{name, fullAddress, usFaker.phoneNumber().cellPhone()};
     }
 }
 
 class firstThread extends Thread {
-    private String[] data;
+    private String[][] data;
     private Faker faker;
     private int n;
     private String region;
-    public firstThread(String[] data, Faker faker,int n, String region){
+    public firstThread(String[][] data, Faker faker,int n, String region){
         this.data = data;
         this.faker = faker;
         this.n = n;
@@ -68,17 +69,21 @@ class firstThread extends Thread {
 
     public void run() {
         for(int i = n/4; i < this.n/2; i++) {
-            this.data[i] = Main.fakeDataUnit(this.faker, this.region);
+            try {
+                    this.data[i] = Main.fakeDataUnit(this.faker, this.region);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 class secondThread extends Thread {
-    private String[] data;
+    private String[][] data;
     private Faker faker;
     private int n;
     private String region;
 
-    public secondThread(String[] data, Faker faker,int n, String region){
+    public secondThread(String[][] data, Faker faker,int n, String region){
         this.data = data;
         this.faker = faker;
         this.n = n;
@@ -87,17 +92,21 @@ class secondThread extends Thread {
 
     public void run() {
         for(int i = 1*n/2; i < 3*this.n/4; i++) {
-            this.data[i] = Main.fakeDataUnit(this.faker, this.region);
+            try {
+                this.data[i] = Main.fakeDataUnit(this.faker, this.region);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 class thridThread extends Thread {
-    private String[] data;
+    private String[][] data;
     private Faker faker;
     private int n;
     private String region;
 
-    public thridThread(String[] data, Faker faker,int n, String region){
+    public thridThread(String[][] data, Faker faker,int n, String region){
         this.data = data;
         this.faker = faker;
         this.n = n;
@@ -106,7 +115,11 @@ class thridThread extends Thread {
 
     public void run() {
         for(int i = 0; i < n/4; i++) {
-            this.data[i] = Main.fakeDataUnit(this.faker,region);
+            try {
+                this.data[i] = Main.fakeDataUnit(this.faker,region);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
